b56483541c4fc383d8267d396c23995dd959532c
andriyansyah0003 <andriyansyah0003@gmail.com>
2022-05-25 09:59:12 +0700

    first version

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.set("view engine", "hbs"); // set the view engine to handlebars
 
 app.use("/public", express.static(__dirname + "/public")); // set the public folder as static
+app.use(express.urlencoded({ extended: false })); // set the body parser
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.get("/add-project", (req, res) => {
   res.render("add-project");
 });
+app.post("/add-project", (req, res) => {
+  const data = req.body;
+
+  res.redirect("/");
+  console.log(data);
+});
+
 app.get("/detail-project", (req, res) => {
   res.render("project-detail");
 });
+
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
